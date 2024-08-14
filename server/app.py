ebcd934a5e313d3b65befc20d84cd4d40f696984
Barasa <timbaros87@gmail.com>
2024-08-14 16:57:00 +0300

    Add vie to earthquake

--- server/app.py
+++ server/app.py
@@ -21,6 +21,17 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.get(id)
+
+    if earthquake:
+        body = earthquake.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
 
 
 if __name__ == '__main__':
